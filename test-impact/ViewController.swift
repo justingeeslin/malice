59287606bfa3b7ef80a61d27b6166b09f5833ea7
Justin Geeslin <justin@geesl.in>
2019-04-16 14:13:13 -0500

    Core Motion init
    My first samples in Core Motion

--- test-impact/ViewController.swift
+++ test-impact/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    let motion = CMMotionManager()
+    var queue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        motion.startDeviceMotionUpdates()
+        startQueuedUpdates()
     }
 
+    func startQueuedUpdates() {
+        print("Starting to get queued updates..")
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = 5.0 / 60.0
+            self.motion.showsDeviceMovementDisplay = true
+            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical,
+                                                 to: self.queue, withHandler: { (data, error) in
+                // Make sure the data is valid before accessing it.
+                if let validData = data {
+                    let maxAccel = abs(validData.userAcceleration.z)
+                    
+                    if (maxAccel > 1) {
+                        print("OUCH! Are you okay?");
+                    }
+                    
+                    
+                }
+            })
+        }
+        else {
+            print("Device Motion not available")
+        }
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+//        motion.stopDeviceMotionUpdates()
+    }
 }
 
